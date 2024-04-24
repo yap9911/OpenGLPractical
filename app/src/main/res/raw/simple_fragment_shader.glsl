0aad1dc7e6d345626f33c8f6cdd575c7ff50d951
yap9911 <75289918+yap9911@users.noreply.github.com>
2024-04-24 20:51:42 +0800

    update the vertex and fragment shader as same as practical 10

--- app/src/main/res/raw/simple_fragment_shader.glsl
+++ app/src/main/res/raw/simple_fragment_shader.glsl
@@ -1,19 +1,47 @@
 
-
-//precision mediump float;
-//void main() {
-//    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-//}
-
 // Set the default precision to medium, don't need as high precision in the fragment shader.
 precision mediump float;
 
-// This is the color from the vertex shader interpolated across the triangle perfragment.
+uniform sampler2D u_Texture; // uniform variable to keep track of the texture coordinate
+// slots to use @ sampling a texture
+
+// This is the color from the vertex shader interpolated across the triangle per fragment.
 varying vec4 v_Color;
+varying lowp vec2 frag_TexCoord;
+varying lowp vec3 frag_Normal;
+varying lowp vec3 frag_Position;
+
+struct Light {
+    vec3 Color;// R, G, B
+    float AmbientIntensity;
+    float DiffuseIntensity;
+    vec3 Direction;
+    float SpecularIntensity;
+    float Shininess;
+};
+
+uniform Light u_Light;
 
 // The entry point for our fragment shader.
 void main()
 {
-    // Pass through the color
-    gl_FragColor = v_Color;
+    // Ambient
+    lowp vec3 AmbientColor = u_Light.Color * u_Light.AmbientIntensity;
+
+    // Diffuse
+    lowp vec3 Normal = normalize(frag_Normal);
+    lowp float DiffuseFactor = max(-dot(Normal, u_Light.Direction), 0.0);
+    lowp vec3 DiffuseColor = u_Light.Color * u_Light.DiffuseIntensity * DiffuseFactor;
+    gl_FragColor = texture2D(u_Texture, frag_TexCoord) * vec4((AmbientColor +
+    DiffuseColor), 1.0);
+
+    // Specular
+    lowp vec3 Eye = normalize(frag_Position);
+    lowp vec3 Reflection = reflect(u_Light.Direction, Normal);
+    lowp float SpecularFactor = pow(max(0.0, -dot(Reflection, Eye)), u_Light.Shininess);
+    lowp vec3 SpecularColor = u_Light.Color * u_Light.SpecularIntensity * SpecularFactor;
+    gl_FragColor = texture2D(u_Texture, frag_TexCoord) * vec4((AmbientColor +
+    DiffuseColor + SpecularColor), 1.0);
+
+
 }
--- app/src/main/res/raw/simple_vertex_shader.glsl
+++ app/src/main/res/raw/simple_vertex_shader.glsl
@@ -1,32 +1,28 @@
-
-
-//attribute vec4 a_Position;
-//void main() {
-//    gl_Position = a_Position;
-//}
-
 uniform highp mat4 u_ProjectionMatrix;
-
 uniform highp mat4 u_ModelViewMatrix;
 
-
-attribute vec4 a_Position;    // Per-vertex position information we will pass in.
-attribute vec4 a_Color;       // Per-vertex color information we will pass in.
+attribute vec4 a_Position; // Per-vertex position information we will pass in. (x, y, z)
+//attribute vec4 a_Color; // Per-vertex color information we will pass in. (R, G, B, alpha)
+attribute vec2 a_TexCoord; // input for texture coordinate (u,v)
+attribute vec3 a_Normal; // input for normal vector
 
 varying vec4 v_Color; // This will be passed into the fragment shader.
+varying vec2 frag_TexCoord;
+varying lowp vec3 frag_Normal; // output to the fragment shader for normal vector
+varying lowp vec3 frag_Position;
 
 // The entry point for our vertex shader.
 void main()
 {
     // Pass through the color.
-    v_Color = a_Color;
+    // v_Color = a_Color;
+
+    frag_Normal = (u_ModelViewMatrix * vec4(a_Normal, 0.0)).xyz;
+    frag_Position = (u_ModelViewMatrix * a_Position).xyz;
 
     // gl_Position is a special variable used to store the final position.
     // the final point in normalized screen coordinates.
-    //gl_Position = a_Position;
-
-    //p8
-    //gl_Position = u_ModelViewMatrix * a_Position;
     gl_Position = u_ProjectionMatrix * u_ModelViewMatrix * a_Position;
 
+    frag_TexCoord = a_TexCoord; // set the output variable to be the input variable
 }
